d5f4a3059dc1b0d8b7489a935392aa5635cad3a7
xcgh-rax-rax <lpatten1729@gmail.com>
2024-07-03 10:20:23 +0100

    turn on ts support in svelet

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -64,6 +64,9 @@ local plugins = {
     {
         'leafOfTree/vim-svelte-plugin',
         ft = { 'svelte' },
+        config = function()
+            vim.g.vim_svelte_plugin_use_typescript = 1
+        end,
     }
 }
 return plugins
